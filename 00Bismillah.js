e033fbabef000cabaf50f1bbe2de5c761d86039e
IbnulMisbah <abdullahibnulmisbah@gmail.com>
2025-01-04 07:28:51 +0600

    My first Js program on github .

---
+++ 00Bismillah.js
@@ -0,0 +1,6 @@
+// Bismillahir Rahmanir Rahim .
+// this is my "Hello world !" program .
+
+console.log("Bismillahir Rahmanir Rahim ..");
+console.log("Assalamu Alaikum !");
+console.log("I am IbnulMisbah .");
